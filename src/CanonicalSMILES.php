53def02baf3fd5fe923a662d4ba529f717c50d52
Kevin Davies <kdavies@gmail.com>
2017-10-18 10:34:48 +1300

    src directory

---
+++ src/CanonicalSMILES.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1); // must be first line
+
+namespace kdaviesnz\canonicalsmiles;
+
+
+
+class CanonicalSMILES
+{
+
+    /**
+     * CanonicalSMILES constructor.
+     */
+    public function __construct()
+    {
+    }
+}
